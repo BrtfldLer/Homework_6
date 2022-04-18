ce6569e8d9d03053df51f853bc6a915843671152
Valeriia <bartfeld.ler@yandex.ru>
2022-04-18 19:59:04 +0200

    Done

---
+++ Task_41/Program.cs
@@ -0,0 +1,25 @@
+﻿// пользователь вводит с клавиатуры М чисел
+// посчитать, сколько чисел больше 0 ввёл пользователь
+
+Console.WriteLine("Введите целые числа через пробел: ");
+string numb = Convert.ToString(Console.ReadLine());
+string[] numbarray = numb.Split(" ");
+int [] ConvertNumb = new int [numbarray.Length];
+for (int i = 0; i < numbarray.Length; i++)
+{
+ConvertNumb[i] = Convert.ToInt32(numbarray[i]);
+}
+int CountNumb(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
+System.Console.Write("Количество чисел больше 0 равно ");
+System.Console.Write(CountNumb(ConvertNumb));
---
+++ Task_43/Program.cs
@@ -0,0 +1,27 @@
+﻿// Программа найдет точку пересечения двух двух прямых, заданных уравнениями y=k1*x+b1; y=k2*x+b2
+// Значения k, b вводятся пользователем
+double numberX = 0;
+double numberY = 0;
+Console.WriteLine("Введите значение переменной k1");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение переменной b1");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение переменной k2");
+int k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение переменной b2");
+int b2 = Convert.ToInt32(Console.ReadLine());
+
+if (k1 == k2 && b1 == b2)
+{
+Console.WriteLine("Прямые не пересекаются");
+}
+else if (k1==k2 && b1!=b2)
+{
+    Console.WriteLine("Прямые не пересекаются");
+}
+else
+{
+numberX = (b2-b1)/(k1-k2);
+numberY = k1*numberX + b1;
+Console.WriteLine($"Координаты точки пересечения двух прямых ({numberX},{numberY})");
+}
